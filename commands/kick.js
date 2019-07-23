6581cfc22d434417002dd27f0d7509494588e66c
Randall Barker <randall.jaren.barker@gmail.com>
2019-07-23 15:03:50 -0600

    Fixed arguments only allowing 1 word of kicking reason

--- commands/kick.js
+++ commands/kick.js
@@ -8,16 +8,16 @@ module.exports = {
     help: 'Kicks specified user from the guild',
     usage: '@user reason',
     doNotDocument: false,
-    level: 9999999999, // Change to Role level to use
+    level: 3, // Change to Role level to use
   },
   fn: function(msg){
-    const prefix = config.prefix;
-    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
     const user = msg.mentions.users.first();
+    const reason = msg.substring(config.prefix.length+4+2,msg.content.length);
+    // prefix + kick + spacing
     if(user){
       const member = msg.guild.member(user);
       if(member){
-        member.kick(`${args[1]}` || `No Reason Provided`).then(() => {
+          member.kick(`${reason}` || `No Reason Provided`).then(() => {
           msg.reply(`Successfully Kicked ${user.tag}`);
         }).catch(err => {
           msg.channel.send(`There was an error kicking ${user.tag}.\nThe following error was thrown:`);
