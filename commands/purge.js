5eac2d92d9d6c182e350ab36cad88c8c5089edd6
Randall Barker <randall.jaren.barker@gmail.com>
2019-11-01 15:29:54 -0600

    Added new purge command.

---
+++ commands/purge.js
@@ -0,0 +1,21 @@
+// purge.js
+// Default Moderation Command
+
+module.exports = {
+  meta: {
+    help: 'Mass delete a specified amount of Messages',
+    usage: 'amount',
+    doNotDocument: false,
+    level: 9999,
+  },
+  fn: function(msg){
+    const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
+    var amount = Number(args[1])
+    if(!amount){msg.channel.send("Please provide an amount of messages to purge."); return};
+    msg.channel.bulkDelete(amount+1).then(function(response){
+      // Anything can be done here. Called once messages are deleted successfully.
+    }).catch(function(error){
+      msg.channel.send("An error occurred while purging messages."); return;
+    });
+  }
+}
