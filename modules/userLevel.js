b26e237a11404c94b1191bc0e799943874e21f05
Randall Barker <randall.jaren.barker@gmail.com>
2020-03-01 01:34:41 -0700

    Updated to most recent, working code.

--- modules/userLevel.js
+++ modules/userLevel.js
@@ -22,8 +22,7 @@ function getIdFromRoleName(roleName,guild){
 module.exports = {
   getLevel: function(message,client){
     const dId = Number(message.author.id);
-    // var roles = message.member.roles;
-    const guild = client.guilds.get(config.serverConfiguration.toString()) || message.guild;
+    const guild = client.guilds.get(server) || message.guild;
     var guildMember = guild.members.get(message.author.id);
     var roles = guildMember.roles;
     var highestLevel = 0;
@@ -34,13 +33,7 @@ module.exports = {
         var roleae = sLevel.Roles[i]; // The name of the role
         if(typeof(roleae) == 'undefined' || null){console.log("Invalid Role"); return};
         try{
-          var target;
-          if(server && server !== 0){
-            target = client.guilds.get(String(server));
-          }else{
-            target = message.guild
-          }
-          if(roles.has(getIdFromRoleName(roleae,target))){ //roleae,message
+          if(roles.has(getIdFromRoleName(roleae,guild))){ //roleae,message
             if (roleLevel > highestLevel){
               highestLevel = roleLevel;
             }
@@ -57,8 +50,7 @@ module.exports = {
         }
       }
     }
-      // Code executed after all above code. Used to tell bot.js the permission level.
-    // if(typeof(highestLevel) != ('number')){throw("highestLevel invalid value. Currently: "+highestLevel+" ("+typeof(highestLevel)+")");};
+    // Code executed after all above code. Used to tell bot.js the permission level.
     if(typeof(highestLevel) != ('number')){highestLevel = 0; throw(`highestLevel invalid value. Currently: ${highestLevel} (${typeof(highestLevel)})`)};
     return highestLevel; // Returns User Level. highestLevel or 0
   }
